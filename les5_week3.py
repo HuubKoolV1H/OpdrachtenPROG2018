7fef4b81b912ef007b36718b03f001a939eb4a63
HuubKoolV1H <43294777+HuubKoolV1H@users.noreply.github.com>
2018-09-18 10:16:26 +0200

    Add files via upload

--- les5_week3.py
+++ les5_week3.py
@@ -1,8 +1,33 @@
-username = input("what is your username?")
-age = int(input("what is your age?"))
 
-if age > 18:
-    print(username, "you can vote!")
-else:
-    print(username, "you cannot vote")
+# opdracht 2
+
+#a
+
+for i in range(0,11,1):
+    print(i)
+
+#b
+
+for i in range(1,10,1):
+    print(i)
+
+#c
+
+for i in range(0,9,2):
+    print(i)
+
+#d
+
+for i in range(1, 10,2):
+    print(i)
+
+#e
+
+for i in range(20,61, 10):
+    print(i)
+
+
+
+
+
 
