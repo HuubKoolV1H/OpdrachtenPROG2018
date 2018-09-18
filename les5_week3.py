0af2e5cc4952145ceb26bdb218f3dc033311caf5
HuubKoolV1H <43294777+HuubKoolV1H@users.noreply.github.com>
2018-09-18 09:53:36 +0200

    Add files via upload

---
+++ les5_week3.py
@@ -0,0 +1,8 @@
+username = input("what is your username?")
+age = int(input("what is your age?"))
+
+if age > 18:
+    print(username, "you can vote!")
+else:
+    print(username, "you cannot vote")
+
